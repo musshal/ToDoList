b3a046c8ec987ff268bcbc51f600608d1ad4b431
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-01 20:22:02 +0700

    refactor: fix after deletion force close and refactor

--- app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
@@ -1,13 +1,13 @@
 package com.dicoding.todoapp.ui.detail
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.data.Task
 import com.dicoding.todoapp.ui.ViewModelFactory
-import com.dicoding.todoapp.ui.list.TaskActivity
 import com.dicoding.todoapp.utils.DateConverter
 import com.dicoding.todoapp.utils.TASK_ID
 import com.google.android.material.textfield.TextInputEditText
@@ -27,20 +27,20 @@ class DetailTaskActivity : AppCompatActivity() {
         detailTaskViewModel = ViewModelProvider(this, factory)[DetailTaskViewModel::class.java]
 
         detailTaskViewModel.setTaskId(taskId)
-
-        detailTaskViewModel.task.observe(this) {
-            findViewById<TextInputEditText>(R.id.detail_ed_title).setText(it.title)
-            findViewById<TextInputEditText>(R.id.detail_ed_description).setText(it.description)
-            findViewById<TextInputEditText>(R.id.detail_ed_due_date)
-                .setText(DateConverter.convertMillisToString(it.dueDateMillis))
-        }
+        detailTaskViewModel.task.observe(this, Observer(this::showDetailTask))
 
         findViewById<Button>(R.id.btn_delete_task).setOnClickListener {
             detailTaskViewModel.deleteTask()
-
-            val taskIntent = Intent(this, TaskActivity::class.java)
-            startActivity(taskIntent)
             finish()
+        }
+    }
+
+    private fun showDetailTask(task: Task?) {
+        if (task != null) {
+            findViewById<TextInputEditText>(R.id.detail_ed_title).setText(task.title)
+            findViewById<TextInputEditText>(R.id.detail_ed_description).setText(task.description)
+            findViewById<TextInputEditText>(R.id.detail_ed_due_date)
+                .setText(DateConverter.convertMillisToString(task.dueDateMillis))
         }
     }
 }
