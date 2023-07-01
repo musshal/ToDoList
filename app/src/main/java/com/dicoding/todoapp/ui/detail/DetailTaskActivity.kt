3490227ef98a1782ec1a9a43f4f96d6acd1f33d7
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-01 18:40:47 +0700

    feat: implement show detail task and delete action

--- app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
@@ -1,10 +1,20 @@
 package com.dicoding.todoapp.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.ui.ViewModelFactory
+import com.dicoding.todoapp.ui.list.TaskActivity
+import com.dicoding.todoapp.utils.DateConverter
+import com.dicoding.todoapp.utils.TASK_ID
+import com.google.android.material.textfield.TextInputEditText
 
 class DetailTaskActivity : AppCompatActivity() {
+
+    private lateinit var detailTaskViewModel: DetailTaskViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,5 +22,25 @@ class DetailTaskActivity : AppCompatActivity() {
 
         //TODO 11 : Show detail task and implement delete action
 
+        val taskId = intent.getIntExtra(TASK_ID, -1)
+        val factory = ViewModelFactory.getInstance(this)
+        detailTaskViewModel = ViewModelProvider(this, factory)[DetailTaskViewModel::class.java]
+
+        detailTaskViewModel.setTaskId(taskId)
+
+        detailTaskViewModel.task.observe(this) {
+            findViewById<TextInputEditText>(R.id.detail_ed_title).setText(it.title)
+            findViewById<TextInputEditText>(R.id.detail_ed_description).setText(it.description)
+            findViewById<TextInputEditText>(R.id.detail_ed_due_date)
+                .setText(DateConverter.convertMillisToString(it.dueDateMillis))
+        }
+
+        findViewById<Button>(R.id.btn_delete_task).setOnClickListener {
+            detailTaskViewModel.deleteTask()
+
+            val taskIntent = Intent(this, TaskActivity::class.java)
+            startActivity(taskIntent)
+            finish()
+        }
     }
 }
