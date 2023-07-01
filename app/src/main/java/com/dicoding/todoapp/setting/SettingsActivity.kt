989979493ffd2eccc6314adb94994cb5ab9fc693
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-02 02:02:17 +0700

    feat: implement notification worker

--- app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.notification.NotificationWorker
+import java.util.concurrent.TimeUnit
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -29,6 +34,24 @@ class SettingsActivity : AppCompatActivity() {
             prefNotification?.setOnPreferenceChangeListener { preference, newValue ->
                 val channelName = getString(R.string.notify_channel_name)
                 //TODO 13 : Schedule and cancel daily reminder using WorkManager with data channelName
+                val notificationWorkRequest =
+                    PeriodicWorkRequestBuilder<NotificationWorker>(1, TimeUnit.MINUTES)
+                        .addTag(channelName)
+                        .build()
+
+                val workManager = WorkManager.getInstance(requireContext())
+
+                if (newValue as Boolean) {
+                    workManager.enqueueUniquePeriodicWork(
+                        channelName,
+                        ExistingPeriodicWorkPolicy.REPLACE,
+                        notificationWorkRequest
+                    )
+                } else {
+                    workManager.cancelAllWorkByTag(channelName)
+                    workManager.pruneWork()
+                }
+
                 true
             }
 
