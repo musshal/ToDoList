75b5ab4c9ade5d129d80d3fa2b56b6baf7e2ab10
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-02 02:02:01 +0700

    feat: implement notification worker

--- app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/todoapp/notification/NotificationWorker.kt
@@ -1,14 +1,21 @@
 package com.dicoding.todoapp.notification
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.dicoding.todoapp.R
 import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.data.TaskRepository
 import com.dicoding.todoapp.ui.detail.DetailTaskActivity
+import com.dicoding.todoapp.utils.DateConverter
 import com.dicoding.todoapp.utils.NOTIFICATION_CHANNEL_ID
 import com.dicoding.todoapp.utils.TASK_ID
 
@@ -32,7 +39,42 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
 
     override fun doWork(): Result {
         //TODO 14 : If notification preference on, get nearest active task from repository and show notification with pending intent
+        val taskRepository = TaskRepository.getInstance(applicationContext)
+        val nearestActiveTask = taskRepository.getNearestActiveTask()
+
+        showAlarmNotification(applicationContext, nearestActiveTask)
+
         return Result.success()
     }
 
+    @SuppressLint("ServiceCast")
+    fun showAlarmNotification(context: Context, task: Task) {
+
+        val channelId = "bobbyNotification"
+
+        val message = context.resources.getString(R.string.notify_content, DateConverter.convertMillisToString(task.dueDateMillis))
+
+        val notificationManagerCompat =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.baseline_notifications_active_24)
+            .setContentTitle(task.title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(getPendingIntent(task))
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "channelName",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            builder.setChannelId(channelId)
+            notificationManagerCompat.createNotificationChannel(channel)
+        }
+
+        val notification = builder.build()
+        notificationManagerCompat.notify(100, notification)
+    }
 }
