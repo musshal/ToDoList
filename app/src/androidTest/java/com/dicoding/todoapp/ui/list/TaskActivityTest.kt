0a1253fe75dcb35493b745ef719d8b2a3fb8910b
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-02 02:03:19 +0700

    test: ui test to validate when user tap add task fab

--- app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
@@ -1,6 +1,38 @@
 package com.dicoding.todoapp.ui.list
 
-//TODO 16 : Write UI test to validate when user tap Add Task (+), the AddTaskActivity displayed
-class TaskActivityTest {
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.ui.add.AddTaskActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 
+//TODO 16 : Write UI test to validate when user tap Add Task (+), the AddTaskActivity displayed
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class TaskActivityTest {
+    @Before
+    fun setup() {
+        ActivityScenario.launch(TaskActivity::class.java)
+        Intents.init()
+    }
+
+    @After
+    fun destroy() {
+        Intents.release()
+    }
+
+    @Test
+    fun shouldDisplayAddTaskActivity() {
+        onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.click())
+        intended(hasComponent(AddTaskActivity::class.java.name))
+    }
 }
