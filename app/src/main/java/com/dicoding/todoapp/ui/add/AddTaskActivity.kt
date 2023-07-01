d62954e759439f086ce55bed6492649c23fa8509
Musthafa Faishal <musthafafaishal@gmail.com>
2023-07-01 20:20:49 +0700

    feat: implement add task

--- app/src/main/java/com/dicoding/todoapp/ui/add/AddTaskActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/ui/add/AddTaskActivity.kt
@@ -6,12 +6,19 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.todoapp.R
+import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.ui.ViewModelFactory
 import com.dicoding.todoapp.utils.DatePickerFragment
+import com.google.android.material.textfield.TextInputEditText
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AddTaskActivity : AppCompatActivity(), DatePickerFragment.DialogDateListener {
+
+    private lateinit var addTaskViewModel: AddTaskViewModel
+
     private var dueDateMillis: Long = System.currentTimeMillis()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +27,8 @@ class AddTaskActivity : AppCompatActivity(), DatePickerFragment.DialogDateListen
 
         supportActionBar?.title = getString(R.string.add_task)
 
+        val factory = ViewModelFactory.getInstance(this)
+        addTaskViewModel = ViewModelProvider(this, factory)[AddTaskViewModel::class.java]
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -31,7 +40,19 @@ class AddTaskActivity : AppCompatActivity(), DatePickerFragment.DialogDateListen
         return when (item.itemId) {
             R.id.action_save -> {
                 //TODO 12 : Create AddTaskViewModel and insert new task to database
-                true
+
+                val edTitle = findViewById<TextInputEditText>(R.id.add_ed_title).text.toString()
+                val edDescription = findViewById<TextInputEditText>(R.id.add_ed_description).text.toString()
+
+                when {
+                    edTitle.isEmpty() -> false
+                    edDescription.isEmpty() -> false
+                    else -> {
+                        addTaskViewModel.insertTask(Task(0, edTitle, edDescription, dueDateMillis))
+                        finish()
+                        true
+                    }
+                }
             }
             else -> super.onOptionsItemSelected(item)
         }
